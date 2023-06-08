eaca8125bbb5e97256505e4e87bd7083fae149fc
Greenball233 <74454079+Greenball233@users.noreply.github.com>
2023-06-08 21:28:24 +0800

    Update Util.cs

--- Assets/Scripts/DRFV/Global/Util.cs
+++ Assets/Scripts/DRFV/Global/Util.cs
@@ -494,7 +494,7 @@ namespace DRFV.Global
             }
             catch (Exception e)
             {
-                Debug.LogError($"无法加载\n{keyword}" + e);
+                Debug.LogError($"无法加载{keyword}\n" + e.Message);
                 texture = new Texture2D(0, 0);
                 if (texture.LoadImage(data))
                 {
